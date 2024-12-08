b854481d67af79ddad8721f1fb9685deb5f084f7
Hannah Yang <hy381@cam.ac.uk>
2024-12-08 23:42:27 +0000

    change plot house prices

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -375,7 +375,7 @@ def find_avg_lsoa_price_in_lad(conn, lad_id, lad_boundaries):
     cur = conn.cursor(pymysql.cursors.DictCursor)
     west, south, east, north = lad_bbox
     cur = conn.cursor(pymysql.cursors.DictCursor)
-    cur.execute(f"select lsoa_id, avg(price) from prices_coordinates_oa_data where latitude between {south} and {north} and longitude between {west} and {east} and date_of_transfer >= 2020-01-01")
+    cur.execute(f"select lsoa_id, avg(price) from prices_coordinates_oa_data where latitude between {south} and {north} and longitude between {west} and {east} and date_of_transfer >= '2020-01-01' group by lsoa_id")
     avg_lsoas_col = cur.fetchall()
     return avg_lsoas_col 
 
