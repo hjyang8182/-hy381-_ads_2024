a793f2419eb93b0e6d5b2fcdfe4e4996891cf983
Hannah Yang <hy381@cam.ac.uk>
2024-12-09 00:57:19 +0000

    fix

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -382,12 +382,15 @@ def find_avg_lsoa_price_in_lad(conn, lad_id, lad_boundaries):
 def plot_avg_lsoa_prices_in_lad(conn, lad_id, lad_boundaries, lsoa_boundaries, transport_gdf):
     avg_lsoa_prices = find_avg_lsoa_price_in_lad(conn, lad_id, lad_boundaries)
     avg_lsoa_prices_df = pd.DataFrame(avg_lsoa_prices)
-    lad_lsoa_ids = list(avg_lsoa_prices_df['lsoa_id'].values)
+    cur = conn.cursor()
+    cur.execute(f"select lsoa_id from oa_translation_data where lad_id = '{lad_id}")
+    lad_lsoa_ids = list(map(lambda x : x['lsoa_id'], cur.fetchall()))
     lsoa_boundaries = lsoa_boundaries[np.isin(lsoa_boundaries['LSOA21CD'], lad_lsoa_ids)]
     lsoa_avg_merged = avg_lsoa_prices_df.merge(lsoa_boundaries[['LSOA21CD', 'geometry']], left_on = 'lsoa_id', right_on = 'LSOA21CD')
     lsoa_avg_merged_gdf = gpd.GeoDataFrame(lsoa_avg_merged, geometry = 'geometry')
     lad_row = lad_boundaries[lad_boundaries['LAD21CD'] == lad_id]
     lad_gdf = gpd.GeoDataFrame({'geometry': lad_row.geometry})
+
     lsoa_avg_merged_gdf = gpd.sjoin(lsoa_avg_merged_gdf, lad_gdf, predicate = 'within')
     transport_gdf = find_transport_bbox(transport_gdf, lad_gdf, 'SUB')
     fig, ax = plt.subplots()
