61311e40a5df62ceec2433a730e2e0133ad985e9
Hannah Yang <hy381@cam.ac.uk>
2024-12-11 19:38:56 +0000

    add car availbility vs pct inc

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -544,6 +544,7 @@ def find_median_pct_inc_after_transport_vs_car_ava(conn, lad_id, transport_gdf, 
         if distance_df is None: 
             continue
         distance_df_grouped = distance_df.groupby(['lsoa_id', 'transport_index'])
+        cur = conn.cursor(pymysql.cursors.DictCursor)
         cur.execute(f"select * from car_availability_data where lsoa_id = '{lsoa_id}'")
         car_availability = cur.fetchall()[0]['no_car_proportion']
         for idx, distance_df in distance_df_grouped: 
